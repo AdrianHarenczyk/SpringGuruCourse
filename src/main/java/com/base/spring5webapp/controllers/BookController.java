8a322dd9d60112b31ffc4454763063ffe52cd5f0
Adrian Harenczyk <adrian.harenczyk@gmail.com>
2019-10-06 13:22:53 +0200

    Add author controller and thymeleaf templates.

--- src/main/java/com/base/spring5webapp/controllers/BookController.java
+++ src/main/java/com/base/spring5webapp/controllers/BookController.java
@@ -4,6 +4,8 @@ import com.base.spring5webapp.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +19,7 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
-    @RequestMapping("/books")
+    @GetMapping("/books")
     public String getBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
 
